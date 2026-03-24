51cbca465da078eaa4dc8f40779a6edf7a271f1e
Aman Yadav <yadavaman2109@gmail.com>
2026-03-24 22:35:26 +0530

    Add files via upload

---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+  bg: "#080c13",
+  bgLight: "#131b26",
+  primary: "#00c3ff",
+  text_primary: "#F2F3F4",
+  text_secondary: "#a9b6c4",
+  card: "#0d131c",
+  card_light: "#151e2b",
+  button: "#00c3ff",
+  white: "#FFFFFF",
+  black: "#000000",
+};
+
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#f0f0f0",
+  primary: "#be1adb",
+  text_primary: "#111111",
+  text_secondary: "#48494a",
+  card: "#FFFFFF",
+  button: "#5c5b5b",
+};
